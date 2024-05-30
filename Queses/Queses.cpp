b010f37c7bc50788bb8a5145cad7a32739987fa6
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-30 10:53:59 +0700

    membuat method remove. jika front = -1 dan menampilkan "Queue underflow" dan "The element deleted from the queue is:" maka antrian kosong

--- Queses/Queses.cpp
+++ Queses/Queses.cpp
@@ -40,6 +40,17 @@ public:
 		}
 		queue_array[REAR] = num;
 	}
+
+	void remove()
+	{
+		//cek apakah antrian kosong
+		if (FRONT == -1)
+		{
+			cout << "Queue underflow\n";
+			return;
+		}
+		cout << "\The element deleted from the queue is: " << queue_array[FRONT] << "\n";
+	}
 };
 
 
